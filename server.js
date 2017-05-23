6ffb26bf09c9778a7b68adaf64d4ad7121ac129f
saeel123 <saeel@dccper.com>
2017-05-23 12:59:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+console.log("server is running");
+
+// app.get('/', function (req, res) {
+//     res.send('hello express');
+// });
+
+var middleware = {
+  requiredAuthentication: function (req, res, next) {
+    console.log('private route hit');
+  },
+  logger: function (req, res, next) {
+    console.log('Request: '+ ' '+ new Date().toString()  +' '  +req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requiredAuthentication, function (req, res) {
+  res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function () {
+  console.log("Express Server is running on port: "+ PORT);
+});
