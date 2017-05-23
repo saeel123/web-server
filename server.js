a31dae6987a5a345fa0555e0ec175aa839f5cc14
saeel123 <saeel@dccper.com>
2017-05-23 13:00:43 +0530

    updated server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requiredAuthentication, function (req, res) {
-  res.send('About Us');
+  res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
